56eb06c4c331e49a573263bdca53ff20130fe46a
Zach McMullen <zachmcmullen04@gmail.com>
2023-08-15 08:36:21 -0400

    [Fix[ spelling

--- data/about.ts
+++ data/about.ts
@@ -1,5 +1,5 @@
 export const about = `Hey, I'm Zach McMullen. 
 I'm an entrepreneur, engineer, and freshman studying Computer Science at Purdue University.
 I am from Las Vegas, Nevada. I am interested in all things, web developemnt, CS, and AI! 
-In my free time I like ot watch and play basketball and listen to music. 
+In my free time I like to watch and play basketball and listen to music. 
 This is my personal website where you can learn all about me!`
