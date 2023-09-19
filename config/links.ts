8873161e30d83f7338c418820e1fb0c2270ff695
Zach McMullen <zachmcmullen04@gmail.com>
2023-09-18 20:07:22 -0400

    [Update] linked in link

--- config/links.ts
+++ config/links.ts
@@ -1,5 +1,5 @@
 export const links = {
   twitter: "https://twitter.com/dzachmcm",
   github: "https://github.com/ZachMcM?tab=repositories",
-  linkedin: "https://www.linkedin.com/in/zach-mcmullen-lv/",
+  linkedin: "https://www.linkedin.com/in/zm-lv/",
 };
